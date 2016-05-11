88d96136e62636e292fb2e32db3067675c2630f8
tw00089923 <auwit0205@gmail.com>
2016-05-11 20:12:22 +0800

    2016

---
+++ client/app.ts
@@ -0,0 +1,33 @@
+import {Component, View, NgZone, provide} from 'angular2/core';
+
+import {bootstrap} from 'angular2-meteor';
+
+import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, APP_BASE_HREF} from 'angular2/router';
+
+import {PartiesList} from 'client/parties-list/parties-list';
+
+import {PartyDetails} from 'client/party-details/party-details';
+
+import 'collections/methods';
+
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'ng2-google-maps/core';
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
+
+@Component({
+    selector: 'app'
+})
+@View({
+    template: '<router-outlet></router-outlet>',
+    directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+    { path: '/', as: 'PartiesList', component: PartiesList },
+    { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+    
+])
+class Socially {}
+
+bootstrap(Socially, [ROUTER_PROVIDERS, ANGULAR2_GOOGLE_MAPS_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
